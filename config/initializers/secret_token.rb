6d1731aba3bb261e7a063472111fd59800c37870
Pawel Szymczykowski <makenai@gmail.com>
2013-03-27 09:12:17 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RunAroundTheBlock::Application.config.secret_token = 'd7c813423676bf61fcbab47043f7b9e57cf70613af57ae4ad9255eaabbeb36c0b8ba7f3b7782e7287e02a8dc2895bc69c6281ff59106bb5eaec10ce810aaaee8'
