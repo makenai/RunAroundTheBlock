b8a04aae205c4f06413fa8b122554a38f7eabe73
Pawel Szymczykowski <makenai@gmail.com>
2013-03-27 13:54:02 -0700

    Migrated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130327184525) do
+ActiveRecord::Schema.define(:version => 20130327204013) do
+
+  create_table "games", :force => true do |t|
+    t.datetime "start_at"
+    t.datetime "ended_at"
+    t.integer  "winner_game_piece_id"
+    t.integer  "current_turn_number"
+    t.datetime "created_at",           :null => false
+    t.datetime "updated_at",           :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "runkeeper_id"
