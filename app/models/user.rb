a0cdda0a2d0e6d48273167299d3883ee5bb9ad37
Shaun Haber <srhaber@gmail.com>
2013-03-27 16:13:22 -0700

    Small refactor

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,16 +7,15 @@ class User < ActiveRecord::Base
 
   def self.from_omniauth(auth)
     user = where(auth.slice('runkeeper_id')).first || create_from_omniauth(auth)
-    user.tap do |user|
-      user.name      = auth['info']['name']
-      user.nickname  = auth['info']['nickname']
-      user.location  = auth['info']['location']
-      user.gender    = auth['extra']['raw_info']['gender']
-      user.image_url = auth['extra']['raw_info']['normal_picture']
-      user.thumb_url = auth['extra']['raw_info']['small_picture'] || auth['extra']['raw_info']['medium_picture']
-      user.token     = auth['credentials']['token']
-      user.save
-    end
+    user.name      = auth['info']['name']
+    user.nickname  = auth['info']['nickname']
+    user.location  = auth['info']['location']
+    user.gender    = auth['extra']['raw_info']['gender']
+    user.image_url = auth['extra']['raw_info']['normal_picture']
+    user.thumb_url = auth['extra']['raw_info']['small_picture'] || auth['extra']['raw_info']['medium_picture']
+    user.token     = auth['credentials']['token']
+    user.save
+    user
   end
 
   def self.create_from_omniauth(auth)
