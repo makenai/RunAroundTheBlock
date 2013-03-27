dcbb516c6d2f78cc67f698d4aa700fe76792fa38
Shaun Haber <srhaber@gmail.com>
2013-03-27 14:05:35 -0700

    Store runkeeper oauth token

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130327184525) do
+ActiveRecord::Schema.define(:version => 20130327210318) do
 
   create_table "users", :force => true do |t|
     t.string   "runkeeper_id"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(:version => 20130327184525) do
     t.string   "thumb_url"
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
+    t.string   "token"
   end
 
 end
