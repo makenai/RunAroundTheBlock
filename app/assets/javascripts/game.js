13bf3c51cd7f86e59333f03faf70903d5a18dfbe
Susan Hinton <shinton@zappos.com>
2013-03-29 10:52:40 -0700

    fixed spelling of yellow

---
+++ app/assets/javascripts/game.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+    function setUpGamePieces() {
+        var markers = $(".marker");
+
+        for (var i = 0; i < makers.length; i += 1) {
+
+        }
+    }
+
+
+    var init = function initGame() {
+
+    };
+});
