6965a1e048cb8f198e04e12d82b4f232b38ae3fa
suziam <suz.hinton@gmail.com>
2013-04-01 01:03:43 -0700

    bug fix for bonus controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -7,6 +7,7 @@ class GamesController < ApplicationController
     ]
     @game_pieces.each do |game_piece|
       turn = game_piece.todays_turn
+      bonuses = turn ? turn.bonuses.collect { |b| b.to_hash } : []
       data = {
         id: game_piece.id,
         name: game_piece.name,
@@ -14,7 +15,7 @@ class GamesController < ApplicationController
         starting_space: turn.try(:starting_space) || 0,
         spaces: turn.try(:spaces) || 0,
         is_current_user: game_piece.users.include?( current_user ),
-        bonuses: turn.bonuses.collect { |b| b.to_hash },
+        bonuses: bonuses,
         players: game_piece.players.collect { |p| { name: p.user.name, id: p.id } }
       }
       @game_data.push( data )
