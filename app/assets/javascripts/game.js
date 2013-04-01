acff0cbb466f5f208dc8dcba9af7012c0b4f9f74
Pawel Szymczykowski <makenai@gmail.com>
2013-04-01 00:30:34 -0700

    Animation WIP

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -10,33 +10,77 @@ $(document).ready(function() {
                 marker = $('#gp' + gp.id);
 
             // find the start and end of the animation
-            var spacePosArr = findPosOfSpace(gp.starting_space, gp.spaces, marker);
-            marker.css({'left': spacePosArr[0], 'top': spacePosArr[1]});
-
-            console.log('sp',spacePosArr);
-
+            var spacePos = findPosOfSpace( gp.starting_space );
+            marker.css( spacePos );
         }
-        // run the animation (will this block? I hope it does)
-        //animateGamePiece(marker);
-    }
-
-    function findPosOfSpace(start, spaces, marker) {
-        var endSpace = parseInt(start, 10) + parseInt(spaces, 10),
-            spaceDiv = $('#space' + endSpace),
-            spacePos = spaceDiv.position(),
-            spacePosArr = [];
-
-            spacePosArr.push(Math.ceil(spacePos.left + (spacePos.left / 2)) - 38),
-            spacePosArr.push(Math.ceil(spacePos.top + (spacePos.top / 2)));
-
-        return spacePosArr;
-    }
-
-    function animateGamePiece(marker) {
 
     }
 
-    //var init = function initGame() {
-        setupGamePieces();
-   // };
+    function findPosOfSpace( space ) {
+        var spaceDiv = $( '#space' + space ),
+            spacePos = spaceDiv.position();
+        return { 
+            top: spacePos.top  + 5, 
+            left: spacePos.left + 5
+        };
+    }
+
+    function showCard( bonus, callback ) {
+        var description = '<p>' + bonus.description + '</p>';
+        if ( bonus.spaces > 0 ) { 
+            description += '<p>Go forward ' + bonus.spaces + ' space.</p>';
+        } else {
+            description += '<p>Go back ' + Math.abs( bonus.spaces ) + ' space.</p>';
+        }
+        $('#card-modal div.card').html( description );
+        $('#card-modal').fadeIn( function() {
+         setTimeout( function() {
+            $('#card-modal').fadeOut( callback );
+         }, 1500 );   
+        });
+    }
+
+    function animateGamePieces() {
+        for (var i = 0, gpLen = gamePieces.length; i < gpLen; i += 1) {
+            var gp = gamePieces[i];
+            animateGamePiece( gp, function( gp ) {
+                $.each( gp.bonuses, function( i, bonus ) {
+                    if ( bonus.type == 'wheel_of_fate' ) {
+                        showWheel( bonus, function() {
+                            var newSpace = bonus.spaces + gp.currentSpace;
+                            moveMarkerTo( gp, newSpace );
+                        });
+                    }
+                    if ( bonus.type == 'card' ) {
+                        return;
+                        showCard( bonus, function() {
+                            var newSpace = bonus.spaces + gp.currentSpace;
+                            moveMarkerTo( gp, newSpace );                            
+                        });
+                    }
+                });
+            });
+        }
+    }
+
+    function moveMarkerTo( gp, space, callback ) {
+        var marker = $('#gp' + gp.id);
+        var endPos = findPosOfSpace( space );
+        marker.animate( endPos, 500, callback ); 
+    }
+
+    function animateGamePiece( gp, callback ) {
+        var endSpace = gp.starting_space + gp.spaces;
+        if ( endSpace > 26 ) {
+            endSpace = 26;
+        }
+        moveMarkerTo( gp, endSpace, function() {
+            gp.currentSpace = endSpace;
+            if ( callback ) 
+                callback(gp);
+        });
+    }
+
+    setupGamePieces();
+    animateGamePieces();
 });
