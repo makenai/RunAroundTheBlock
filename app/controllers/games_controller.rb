931d063084836a1876a21e8c86dcc254c65ea69e
Shaun Haber <srhaber@gmail.com>
2013-04-03 15:29:03 -0700

    Use a ternary operator for a turn (ary) variable)

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -14,7 +14,7 @@ class GamesController < ApplicationController
         starting_space: turn.try(:starting_space) || 0,
         spaces: turn.try(:spaces) || 0,
         is_current_user: game_piece.users.include?( current_user ),
-        bonuses: turn.bonuses.collect { |b| b.to_hash },
+        bonuses: turn ? turn.bonuses.collect { |b| b.to_hash } : [],
         players: game_piece.players.collect { |p| { name: p.user.name, id: p.id } }
       }
       @game_data.push( data )
